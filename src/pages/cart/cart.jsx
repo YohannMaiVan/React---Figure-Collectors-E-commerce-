ef7b615eebebb0783cf29615d900b1cfaf8904e3
JinRose <jinrose@Air-van-MacBook.lan>
2023-03-14 17:03:59 +0100

    [ADD] navigation checkout

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -3,10 +3,13 @@ import { PRODUCTS } from "../../products";
 import { ShopContext } from "../../context/shop-context";
 import { CartItem } from "./cart-item";
 import "./cart.css";
+import { useNavigate } from "react-router-dom";
 
 export const Cart = () => {
   const { cartItems, getTotalCartPrice } = useContext(ShopContext);
   const totalPrice = getTotalCartPrice();
+
+  const navigate = useNavigate();
 
   return (
     <div className="cart">
@@ -22,11 +25,18 @@ export const Cart = () => {
         })}
       </div>
 
-      <div className="checkout">
-        <p> Total: {totalPrice}$</p>
-        <button>Continue Shopping</button>
-        <button>Checkout</button>
-      </div>
+      {totalPrice > 0 ? (
+        <div className="checkout">
+          <p> Total: {totalPrice}$</p>
+          <button onClick={() => navigate("/")}>Continue Shopping</button>
+          <button>Checkout</button>
+        </div>
+      ) : (
+        <div className="checkout">
+          <h1>Your cart is empty</h1>
+          <button onClick={() => navigate("/")}>Go Shopping !</button>
+        </div>
+      )}
     </div>
   );
 };
